048c8d0551e5efe3d3de558d69972a77b5a16e82
Bas Weelinck <bas@weelinck.org>
2019-09-08 00:40:50 +0200

    Add simple HTTP server example.

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "net/http"
+    "html"
+    "fmt"
+    "log"
+)
+
+func main() {
+    http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
